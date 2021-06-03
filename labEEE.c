36ef982c366cc1e91f5b9225958e08ea3882e722
Vikhyathraj Shetty K <itsvikhyathraj@gmail.com>
2021-06-03 23:03:01 +0530

    added printf statments program

---
+++ labEEE.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main() {
+    printf("Have\na Good day");
+    printf("\n");
+
+    printf("Have/\\a /\\Good /\\ day!");
+    printf("\n");
+
+    printf("Laughter is the best \"Medicine\". Share the \"Knowledge\"");
+    printf("\n");
+
+    printf("Hello\n\tWorld!");
+    printf("\n");
+
+    printf("Hello\n\t\tWorld!");
+    printf("\n");
+
+    printf("How are you?\n\tI am Fine.");
+    printf("\n");
+
+    printf("How are you?\nI\n");
+    printf("am Fine.");
+    printf("\n");
+
+    printf("How\tare\tyou?\nI\tam\tFine.\tThank\tYou.");
+    printf("\n");
+
+    printf("Something has gone crazy \a");
+    printf("\n");
+    
+    printf("Bank interest is 9%% in the year 2018");
+    printf("\n");
+}
