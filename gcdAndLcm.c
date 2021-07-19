aac10870caeb33661c7045dc8ca628be1fcca5e0
Vikhyathraj Shetty K <itsvikhyathraj@gmail.com>
2021-07-19 10:10:34 +0530

    updated GCD and LCM program

--- gcdAndLcm.c
+++ gcdAndLcm.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 
 int gcd(int n1, int n2) {
-    if(n1==0) return n2;
-    if(n2==0) return n1;
-    if(n1==n2) return n1;
+    // if(n1==0) return n2;
+    // if(n2==0) return n1;
+    // if(n1==n2) return n1;
 
-    if(n1>n2) {
-        return gcd(n1-n2, n2);
-    } else {
-        return gcd(n1, n2-n1);
-    }
+    // if(n1>n2) {
+    //     return gcd(n1-n2, n2);
+    // } else {
+    //     return gcd(n1, n2-n1);
+    // }
+    if(n2==0) return n1;
+    return gcd(n2, n1%n2);
 }
 
 int lcm(int m1, int m2) {
